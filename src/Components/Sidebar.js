8703389b5604c6870d22d53d86d8ca9154d32e5d
aniruddhparwal <aniruddhparwal@gmail.com>
2020-10-19 23:47:22 +0530

    Side Bar header

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Chat, DonutLarge, MoreVert } from '@material-ui/icons'
+import { Avatar, IconButton } from '@material-ui/core'
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            I am sidebar
+            <div className="sidebar__header">
+                <div className="sidebar__left">
+                    <Avatar />
+                </div>
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLarge />
+                    </IconButton>
+                    <IconButton>
+                        <Chat />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
         </div>
     )
 }

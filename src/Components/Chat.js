a471eda3eefe974c867a383152708027e8cb83fe
aniruddhparwal <aniruddhparwal@gmail.com>
2020-10-22 22:13:53 +0530

    chat section

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,9 +1,28 @@
+import { Avatar, IconButton } from '@material-ui/core'
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons'
 import React from 'react'
 
 function Chat() {
     return (
         <div className="chat">
-            I am  Chat
+            <div className="chat__header">
+                <Avatar />
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                    <p>Last Seen at...</p>
+                </div>
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
         </div>
     )
 }

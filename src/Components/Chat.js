f755ccda0178b2154170816a609000d9338656c5
aniruddhparwal <aniruddhparwal@gmail.com>
2020-10-26 22:51:08 +0530

    UI completed

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,8 +1,11 @@
 import { Avatar, IconButton } from '@material-ui/core'
-import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons'
+import { AttachFile, InsertEmoticon, Mic, MoreVert, SearchOutlined, SettingsInputAntenna } from '@material-ui/icons'
 import React from 'react'
+import { useState } from 'react'
 
 function Chat() {
+    const [input, setInput] = useState(' ')
+
     return (
         <div className="chat">
             <div className="chat__header">
@@ -22,6 +25,48 @@ function Chat() {
                         <MoreVert />
                     </IconButton>
                 </div>
+            </div>
+            <div className="chat__body">
+                <p className="chat__message">
+                    <span className="chat__name">
+                        ANi
+                    </span>
+                    mess
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+
+                <p className="chat__reciever chat__message">
+                    <span className="chat__name">
+                        ANi
+                    </span>
+                    mess
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+
+                <p className="chat__message">
+                    <span className="chat__name">
+                        ANi
+                    </span>
+                    mess
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+            </div>
+            <div className="chat__footer">
+                <InsertEmoticon />
+                <form>
+                    <input value={input}
+                        onChange={e => { setInput(e.target.value) }}
+                        placeholder="Type a message"
+                        type="text" />
+                    <button type="submit">Send a message</button>
+                </form>
+                <Mic />
             </div>
         </div>
     )
